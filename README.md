032a8be19ea3669697b3b2efabdbef2ffaf00399
gbdb123 <747807389@qq.com>
2017-12-15 22:51:25 +0800

    cm-14.1

---
+++ README.md
@@ -0,0 +1,23 @@
+Copyright 2017 - The LineageOS Project
+
+Device configuration for 360 N4S (_QK1505_)
+=====================================================
+
+Basic   | Spec Sheet
+-------:|:-------------------------
+CPU     | Octa-core 2.0 GHz Cortex-A53
+CHIPSET | Qualcomm MSM8953 Snapdragon 625
+GPU     | Adreno 506
+Memory  | 4 GB
+Shipped Android Version | Android 6.0.1 with 360OS 2.0
+Storage | 64 GB
+MicroSD | Up to 256 GB (Hybrid)
+Battery | 5060 mAh (non-removable)
+Dimensions | 141.3 x 69.6 x 8.9 mm
+Display | 1080 x 1920 pixels, 5.0" IPS
+Rear Camera  | 16.0 MP, Dual LED flash
+Front Camera | 8.0 MP
+FingerPrint | Yes
+Release Date | November 2016
+
+![360 N4S QCOM](https://imgsa.baidu.com/baike/c0%3Dbaike80%2C5%2C5%2C80%2C26/sign=657dc0c79f0a304e462fa8a8b0a1cce3/4d086e061d950a7b9a6afa0702d162d9f3d3c992.jpg "360 N4S QCOM")
---
+++ lineage.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, device/360/QK1505/full_QK1505.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_QK1505
+BOARD_VENDOR := 360
+
+PRODUCT_GMS_CLIENTID_BASE := android-360
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=360/QK1505/QK1505:6.0.1/MMB29M/6.0.042.P0.161110.QK1505:user/release-keys \
+    PRIVATE_BUILD_DESC="QK1505-user 6.0.1 MMB29M V42 release-keys"
